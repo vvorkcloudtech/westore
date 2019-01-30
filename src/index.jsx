670780b0d434ff687514bc7359f1096565f6d9c5
hamzabutt32 <hamza.butt32@hotmail.com>
2019-01-30 15:57:16 +0500

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./js/components/App";
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("app")
+);
